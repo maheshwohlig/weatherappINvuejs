e32fd254ec10cb3da1ec2ed9570e4b620137e2e7
maheshwohlig <maheshwohlig@gmail.com>
2021-12-17 18:44:04 +0530

    day1

---
+++ src/components/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1>hello</h1>
+    <div class="container">
+      <div class="product" v-for="product in products" :key="product.id">
+        {{ product.name }},{{ product.id }}
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import { mapGetters } from "vuex";
+export default {
+  name: "Home",
+  computed: mapGetters(["products"]),
+};
+</script>
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+}
+.container {
+  height: 100hv;
+  display: flex;
+  grid-gap: 1rem;
+}
+.product {
+  width: 10%;
+  padding: 1rem;
+  text-align: center;
+  position: relative;
+  background-color: black;
+  color: white;
+  border-radius: 10px;
+}
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,18 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    todos: [
+      { id: 1, name: 'mahesh' },
+      { id: 2, name: 'bunny' },
+      { id: 3, name: 'micky' },
+      { id: 4, name: 'rohit' },
+      { id: 5, name: 'rahul' },
+      { id: 6, name: 'jeck' },
+      { id: 7, name: 'harry' },
+    ],
+  },
+  getters: {
+    products: (state) => state.todos
   },
   mutations: {
   },
