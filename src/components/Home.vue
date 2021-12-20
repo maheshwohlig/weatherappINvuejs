25af8e12c9fd72fe1932c7ee887fc9672eacf599
maheshwohlig <maheshwohlig@gmail.com>
2021-12-20 18:43:02 +0530

    changed

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,30 +1,98 @@
 <template>
   <div>
-    <h1>hello</h1>
-    <div class="container">
-      <div class="product" v-for="product in products" :key="product.id">
-        {{ product.name }},{{ product.id }}
+    <div>
+      <v-toolbar>
+        <v-text-field
+          hide-details
+          prepend-icon="mdi-magnify"
+          single-line
+          id="data"
+          v-model="seach"
+        ></v-text-field>
+        <v-btn v-on:click="add" color="red">
+          <v-icon dark> mdi-plus </v-icon>
+        </v-btn>
+      </v-toolbar>
+    </div>
+    <div class="container" justify-center>
+      <div v-for="product in whether" :key="product">
+        <!-- <h1>{{ product.name }},{{ product.sys.country }}</h1>
+
+        <h3>{{ Math.round(product.main.temp) - 273 }}</h3>
+        <p>{{ Math.round(product.main.temp_min) - 273 }}</p>
+
+        <p>{{ Math.round(product.main.temp_max) - 273 }}</p> -->
+        {{ product }}
       </div>
     </div>
   </div>
 </template>
+
 <script>
 import { mapGetters } from "vuex";
+import { mapActions } from "vuex";
 export default {
   name: "Home",
+  data() {
+    return {
+      seach: "mumbai",
+      whether: [],
+    };
+  },
+
   computed: mapGetters(["products"]),
+  methods: {
+    ...mapActions(["fetchdata"]),
+    add() {
+      if (this.whether.length === 0) {
+        this.whether.push(this.seach);
+      }
+
+      let getValue = this.whether.includes(this.seach);
+
+      // let getValue = this.whether.every((value) => {
+      //   console.log("<<<<<<" + value);
+      //   console.log("this.seach" + this.seach);
+      //   if (value !== this.seach) {
+      //     // return this.seach;
+      //     return true;
+
+      //     // console.log("this.seach......" + this.seach);
+      //   }
+      // });
+      console.log("getValue..." + getValue);
+      if (getValue === false) {
+        this.whether.push(this.seach);
+      }
+      console.log("<<<<<<<" + this.seach);
+      this.fetchdata(this.whether);
+    },
+  },
+
+  created() {
+    this.fetchdata(this.seach);
+  },
 };
 </script>
+
 <style scoped>
 * {
   margin: 0;
   padding: 0;
 }
+
 .container {
-  height: 100hv;
+  width: 100%;
+  height: 30%;
+  position: absolute;
   display: flex;
-  grid-gap: 1rem;
+  background-color: yellow;
+  align-content: space-around;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 }
+
 .product {
   width: 10%;
   padding: 1rem;
--- src/store/index.js
+++ src/store/index.js
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import Vue from 'vue'
 import Vuex from 'vuex'
 
@@ -5,22 +6,35 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    todos: [
-      { id: 1, name: 'mahesh' },
-      { id: 2, name: 'bunny' },
-      { id: 3, name: 'micky' },
-      { id: 4, name: 'rohit' },
-      { id: 5, name: 'rahul' },
-      { id: 6, name: 'jeck' },
-      { id: 7, name: 'harry' },
+    weathers: [
+      // { id: 1, name: 'mahesh' },
+      // { id: 2, name: 'bunny' },
+      // { id: 3, name: 'micky' },
+      // { id: 4, name: 'rohit' },
+      // { id: 5, name: 'rahul' },
+      // { id: 6, name: 'jeck' },
+      // { id: 7, name: 'harry' },
     ],
   },
   getters: {
-    products: (state) => state.todos
-  },
-  mutations: {
+    products: (state) => state.weathers
   },
   actions: {
+    async fetchdata({ commit }, seach) {
+      console.log(seach);
+      const result = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${seach}&appid=e6dbb018942b5cd4d196d42e1886c8fa`);
+
+      commit('setdata', result.data);
+    },
+    // async newdata({ commit }, seach) {
+    //   const response = await axios.post(`https://api.openweathermap.org/data/2.5/weather?q=${seach}&appid=e6dbb018942b5cd4d196d42e1886c8fa`, { seach, completed: false });
+
+    //   commit('newdata', response.data);
+    // }
+  },
+  mutations: {
+    setdata: (state, weathers) => (state.weathers = weathers),
+    // newdata: (state, weathers) => state.weathers.push(weathers),
   },
   modules: {
   }
